cb187d46bcab59b24fcf767a2e96a4ca9237991f
Cevado <flavio.mv@gmail.com>
2017-06-11 10:56:07 -0300

    Concludes external API for Unidecode
    This finishes the code for the functions unidecode/1 and decode/1
    At this point all the structure is fully usable missing only the
    csv files for each unicode chart

--- lib/unidecode.ex
+++ lib/unidecode.ex
@@ -1,14 +1,33 @@
 defmodule Unidecode do
   @moduledoc """
+  #TODO doc
   """
 
   @doc """
+  #TODO doc
   """
-  def unidecode(string) do
+  @spec unidecode(String.t) :: String.t
+  def unidecode(string), do: transliterate(string)
+
+  @doc """
+  #TODO doc
+  """
+  @spec unidecode(String.t) :: String.t
+  def decode(string), do: transliterate(string)
+
+  @spec unidecode(String.t) :: String.t
+  defp transliterate(string) do
     string
+    |> String.graphemes
+    |> Stream.map(&transliterate_char/1)
+    |> Enum.join
   end
 
-  def decode(string) do
-    string
+  @spec unidecode(String.t) :: String.t
+  defp transliterate_char(char) do
+    case Unidecode.Cache.get(char) do
+      nil -> char
+      new_char -> new_char
+    end
   end
 end
