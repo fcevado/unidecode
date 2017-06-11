58f596c5427c2f2e996b19c83bbaf546d3df8a53
Cevado <flavio.mv@gmail.com>
2017-06-11 12:02:17 -0300

    Update Mixfile with all project info
    Adds package keyword, description and source_url to project keyword
    as metadata for hex.pm

--- mix.exs
+++ mix.exs
@@ -3,16 +3,36 @@ defmodule Unidecode.Mixfile do
 
   def project do
     [app: :unidecode,
-     version: "0.1.0",
+     version: "0.0.1",
      elixir: "~> 1.4",
+     description: description(),
+     source_url: "https://github.com/fcevado/unidecode",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     package: package(),
      deps: deps()]
   end
 
   def application do
     [extra_applications: [:logger, :con_cache],
      mod: {Unidecode.Application, []}]
+  end
+
+  defp description do
+    """
+    Transliterate from Unicode to ASCII.
+    Based upon [Text::Unidecode](http://search.cpan.org/~sburke/Text-Unidecode-1.30/lib/Text/Unidecode.pm) perl module.
+    """
+  end
+
+  defp package do
+    [
+      name: :unidecode,
+      files: ["priv", "lib/unidecode.ex", "lib/unidecode", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Flávio Moreira Vieira"],
+      license: ["Apache 2.0"],
+      links: %{"Github" => "https://github.com/fcevado/unidecode"}
+    ]
   end
 
   defp deps do
