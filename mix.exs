37174eabe2dfd39f20a081e2c6527d22314efe5a
Pedro Vieira <pedro.vieira@phishx.io>
2018-06-25 11:32:58 -0300

    v0.0.2

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule Unidecode.Mixfile do
     [
       name: :unidecode,
       files: ["priv", "lib/unidecode.ex", "lib/unidecode", "mix.exs", "README.md", "LICENSE"],
-      maintainers: ["Flávio Moreira Vieira"],
+      maintainers: ["Flávio Moreira Vieira", "Pedro Vieira"],
       licenses: ["Apache 2.0"],
       links: %{
         "Github" => "https://github.com/fcevado/unidecode",
@@ -42,8 +42,8 @@ defmodule Unidecode.Mixfile do
   defp deps do
     [
       {:con_cache, ">= 0.13.0"},
-      {:ex_doc, "~> 0.16", only: :dev},
-      {:earmark, "~> 1.2", only: :dev}
+      {:ex_doc, ">= 0.18.0", only: :dev},
+      {:earmark, ">= 1.2.0", only: :dev}
     ]
   end
 end
